8249031f00b983b57f3acb99548c3284145d7fec
dsmabulage <dileepainivos@gmail.com>
2023-11-13 22:01:03 +0530

    add auth with backend env

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,44 +1,48 @@
+import { useAuth } from '@clerk/clerk-react';
 import React from 'react';
-import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
-import logo from '../../assets/images/cyberpunk.svg'
+import { Button, Nav, NavDropdown, Navbar } from 'react-bootstrap';
+import logo from '../../assets/images/cyberpunk.svg';
 
 const NavBar = () => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Navbar.Brand href="#home">
-                <img
-                    alt=""
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />{' '}
-                <div className='navbar-title'>
-                    <span className='main-title'>Cyberpunk</span>
-                    <span className='subtitle'>Software project</span>
-                </div>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto navbar-section-1">
-                    <Nav.Link href="#home">CP - Scrum board</Nav.Link>
-                    <NavDropdown title="Sprints" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Sprint 01</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Sprint 02</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Sprint 03</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Nav className='navbar-section-2'>
-                    <div>
-                        
-                    </div>
-                    <div>
-
-                    </div>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
+  const { signOut } = useAuth();
+  return (
+    <Navbar expand='lg' className='bg-body-tertiary'>
+      <Navbar.Brand href='#home'>
+        <img
+          alt=''
+          src={logo}
+          width='30'
+          height='30'
+          className='d-inline-block align-top'
+        />{' '}
+        <div className='navbar-title'>
+          <span className='main-title'>Cyberpunk</span>
+          <span className='subtitle'>Software project</span>
+        </div>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='me-auto navbar-section-1'>
+          <Nav.Link href='#home'>CP - Scrum board</Nav.Link>
+          <NavDropdown title='Sprints' id='basic-nav-dropdown'>
+            <NavDropdown.Item href='#action/3.1'>Sprint 01</NavDropdown.Item>
+            <NavDropdown.Item href='#action/3.2'>Sprint 02</NavDropdown.Item>
+            <NavDropdown.Item href='#action/3.3'>Sprint 03</NavDropdown.Item>
+          </NavDropdown>
+          <Button
+            variant='outline-danger'
+            onClick={async () => await signOut()}
+          >
+            Sign Out
+          </Button>
+        </Nav>
+        <Nav className='navbar-section-2'>
+          <div></div>
+          <div></div>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default NavBar;
